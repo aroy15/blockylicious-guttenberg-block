27633ebad8081ddd2abdf17ff33ee592c38ae032
aroy15 <anjonroy57@gmail.com>
2024-12-06 15:47:36 +0600

    Flip Horizontally & Vertically toogle added

--- build/blocks/curvy/index.asset.php
+++ build/blocks/curvy/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'bb865ad3774bf35d5961');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '00e54dfe6a5c899dfdf5');
