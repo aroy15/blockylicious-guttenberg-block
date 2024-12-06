34a6563a10761da46643f9112c9b8fd01130bb1a
aroy15 <anjonroy57@gmail.com>
2024-12-06 12:37:56 +0600

    curvy: enabled link & text colors, spacing, default style attribute for backround

--- build/blocks/curvy/index.asset.php
+++ build/blocks/curvy/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '309aa54450f44f36997b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '38f98823a0e87fdce794');
