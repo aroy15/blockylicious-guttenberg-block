8cb5e8f73582eb54fa2853edea624571cbc68407
aroy15 <anjonroy57@gmail.com>
2024-12-07 20:55:19 +0600

    Categorized the Curvy block in the first of the category list

--- blockylicious.php
+++ blockylicious.php
@@ -25,7 +25,23 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
+
+function create_custom_block_category($categories) {
+
+	array_unshift($categories, [
+		'slug' => 'blockylicious',
+		'title' => 'Blockylicious'
+	]);
+
+	// // Category will show in the last
+	// array_push($categories, [
+	// 	'slug' => 'blockylicious',
+	// 	'title' => 'Blockylicious'
+	// ]);
+	return $categories;
+}
 function create_block_blockylicious_block_init() {
+	add_filter('block_categories_all', 'create_custom_block_category');
 	register_block_type( __DIR__ . '/build/blocks/curvy' );
 }
 add_action( 'init', 'create_block_blockylicious_block_init' );
--- build/blocks/curvy/index.asset.php
+++ build/blocks/curvy/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '92724be124945ac02033');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '07d32de43bbbe298e117');
