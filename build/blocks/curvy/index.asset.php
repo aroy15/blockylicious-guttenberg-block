5a4a55325304030a0255fc3c0346f8049967b735
aroy15 <anjonroy57@gmail.com>
2024-12-07 22:54:04 +0600

    preview/example applied on the curvy block

--- build/blocks/curvy/index.asset.php
+++ build/blocks/curvy/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'd4abfddc087921bbc136');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '85a389cf7de5de472fad');
