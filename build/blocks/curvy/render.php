73ab5e951d94b7de65b122bd7c6a4004841aa523
aroy15 <anjonroy57@gmail.com>
2024-12-07 14:19:59 +0600

    Curvy: block content showing in the front-end

--- build/blocks/curvy/render.php
+++ build/blocks/curvy/render.php
@@ -1,5 +1,11 @@
 <?php
-$block_wrapper_attributes = get_block_wrapper_attributes();
+$block_wrapper_attributes = get_block_wrapper_attributes([
+	'class' => 'alignfull'
+]);
+//wp_send_json( $attributes );
 //wp_send_json( $block_wrapper_attributes );
+// wp_send_json( $content );
 ?>
-<div <?php echo $block_wrapper_attributes;?>>This is a test</div>
+<div <?php echo $block_wrapper_attributes;?>>
+	<?echo $content;?>
+</div>
