9b5625b95ca2510ac89ad7824645961cbde51be9
aroy15 <anjonroy57@gmail.com>
2024-12-07 21:13:54 +0600

    custom icon added for curvy block

--- build/blocks/curvy/index.asset.php
+++ build/blocks/curvy/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '07d32de43bbbe298e117');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c20237f8bb6d7cc405a8');
