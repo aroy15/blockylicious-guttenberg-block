b7a400ba05defa54be428035a2b2f2d3ad873925
aroy15 <anjonroy57@gmail.com>
2024-12-07 22:46:02 +0600

    curvy block two variations created 'curvy - top only' & 'curvy - bottom only'

--- build/blocks/curvy/index.asset.php
+++ build/blocks/curvy/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '0575b5eaa502a70a98bf');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'd4abfddc087921bbc136');
