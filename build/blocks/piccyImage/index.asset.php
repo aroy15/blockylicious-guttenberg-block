b5bc41aab7b8a61e36cba31fca0a91594278b171
aroy15 <anjonroy57@gmail.com>
2024-12-10 19:29:31 +0600

    remoe deafult background from the curvly block as new child theme created for colors & other things

--- build/blocks/piccyImage/index.asset.php
+++ build/blocks/piccyImage/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-i18n'), 'version' => '93b47038e962afaddd73');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-i18n'), 'version' => '78748b24cfc0d92367e3');
