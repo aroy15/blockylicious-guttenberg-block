ca06d0076b23ea1fecb3314d9c0f2718c63eaf0d
aroy15 <anjonroy57@gmail.com>
2024-12-10 17:22:49 +0600

    Error preventing in the front-end, if no image selected in the piccyGallery/piccyImage

--- build/blocks/piccyImage/index.asset.php
+++ build/blocks/piccyImage/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-i18n'), 'version' => 'fce4e52a0b61c8863b46');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-i18n'), 'version' => 'e13fd13d647df87b1975');
