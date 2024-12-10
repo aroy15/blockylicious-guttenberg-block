f5e06c1685dd834a65a080dcd280b6baab455099
aroy15 <anjonroy57@gmail.com>
2024-12-10 19:31:09 +0600

    reduced padding left & right for curvy block

--- build/blocks/curvy/index.asset.php
+++ build/blocks/curvy/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '00f4f45fa3266e1c015b');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '5d75428a5d9a111a4f4b');
--- build/blocks/piccyImage/index.asset.php
+++ build/blocks/piccyImage/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-i18n'), 'version' => '78748b24cfc0d92367e3');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-i18n'), 'version' => '2a338773ed5faf3ddbb2');
